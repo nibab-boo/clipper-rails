8dc75d4b2ecbf26405446611273bd415af1fa4bf
Babin Bohora <bohorababin1@gmail.com>
2022-07-23 16:58:46 +0900

    routes for auth and callback

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+
+  get "/oauth/callback", to: "document_callbacks#oauth_callback"
+
+  get "/oauth", to: "document_callbacks#oauth"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
