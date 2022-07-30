dac3d42253bb27da91084dd4f9dcc72b40fc4eb0
Babin Bohora <bohorababin1@gmail.com>
2022-07-31 00:45:05 +0900

    migration migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_30_150332) do
+ActiveRecord::Schema.define(version: 2022_07_30_154312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2022_07_30_150332) do
     t.string "last_name"
     t.string "token"
     t.datetime "token_expiry"
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
